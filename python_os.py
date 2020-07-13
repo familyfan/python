3726b08ef1dfe25c8914e5372cd4700b3d385852
fanliming <2836974148@qq.com>
2020-07-13 20:54:40 +0800

    os

---
+++ python_os.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import tensorflow as tf
+import numpy as np
+import os 
+
+"os.walk"
+#for root, dirs, files in os.walk(".", topdown=False):
+#    for name in files:
+#        print(os.path.join(root, name))
+#    for name in dirs:
+#        print(os.path.join(root, name))
+
+
+
+"os.path.basename()"
+#返回path最后的文件名。若path以/或\结尾，那么就会返回空值
+#例如:
+#path='D:\CSDN'
+#os.path.basename(path)=CSDN
+
+#os.path.basename('c:\csv')
+#'csv' # 这里csv被当作文件名处理了
+#
+#os.path.basename('c:\csv\')
+#'' 返回空
+
+
+
+
+
+
+
+
+
+
+
+
+
+
